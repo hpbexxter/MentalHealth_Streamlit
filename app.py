c26193cf5a1934d68453f1c4bcfde309e2484bfe
hpbexxter <mail@hannes.pralle.de>
2026-06-17 15:44:30 +0200

    feat(nav): added navigation

--- app.py
+++ app.py
@@ -12,17 +12,27 @@ st.set_page_config(
 def load_data():
     return pd.read_csv('./data/mental_health_burnout_tech_2026_cleaned.csv')
 
-# Daten vorladen und in session_state ablegen
+# Load data into session state
 if "df" not in st.session_state:
     st.session_state["df"] = load_data()
 
-# Navigation
-landing = st.Page("pages/landing_page.py",    title="Übersicht", icon="🏠")
-analyse   = st.Page("pages/analyse_page.py", title="Analyse",   icon="📊")
+st.logo(
+    "assets/navbar_logo.svg",
+    size="large",
+    )
 
+# Navigation setup
+landing = st.Page("pages/landing_page.py", title="Übersicht", icon="🏠")
+analyse = st.Page("pages/analyse_page.py", title="Analyse",   icon="📊")
+ml = st.Page("pages/maschine_learning_page.py", title="Maschine Learning", icon="⚙")
+visu = st.Page("pages/visu_page.py", title="Visualisierung", icon="📈")
+
+
+# Create navigation
 pg = st.navigation({
     "Allgemein": [landing],
-    "Daten":     [analyse],
+    "Daten & Analyse": [analyse, visu, ml],
 })
 
+# Run the app
 pg.run()
