3319cabf16dc1fba3342384658cd553c2a5ee434
hpbexxter <mail@hannes.pralle.de>
2026-06-17 15:43:19 +0200

    feat(pages): added some default pages

--- pages/analyse_page.py
+++ pages/analyse_page.py
@@ -2,3 +2,4 @@ import streamlit as st
 
 st.title("Analyse")
 st.write("Hier kommt der Seiteninhalt.")
+
---
+++ pages/visu_page.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Visu-Seite")
+st.write("Hier kommt der Seiteninhalt.")
