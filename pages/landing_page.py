58595da87018be1f70231824b8909c86447195db
hpbexxter <mail@hannes.pralle.de>
2026-06-18 14:54:13 +0200

    feat(content): add some more project specific content and a dataset overview

--- pages/landing_page.py
+++ pages/landing_page.py
@@ -1,25 +1,158 @@
 import streamlit as st
+import plotly.express as px
 
 from utils.utils import get_base64_encoded_image
 
 df = st.session_state["df"]
 
-st.title("Mental Health in der Tech-Branche")
-st.dataframe(df.head())
-
+st.title("🧠 Mental Health in der Tech-Branche")
+st.subheader("📊 Datenanlyse und ML-Vorhersage")
+st.markdown("""
+            Der vorliegende Datensatz befasst sich mit Angaben der psychischen
+            Gesundheit von Mitarbeiter*innen in der Tech-Branche.\n
+            Mit ca. 90.000 Datensätzen, Mitarbeiter*innen aus 10 verschiedenen Ländern und Branchen bietet er
+            eine breite, internationale Grundlage für die Analyse von Burnout
+            und psychischen Belastungen am Arbeitsplatz. Erfasst werden dabei
+            klinisch etablierte Messinstrumente wie der PHQ-9 (zur Erfassung
+            von Depressionssymptomen) die eine fundierte Einschätzung des psychischen
+            Wohlbefindens ermöglichen. 
+            
+            Grundsätzlich wird versucht auf Basis von Berufsrolle und hierarchischer Position
+            im Unternehmen depressive Symptome (PHQ-9) bei Tech-Beschäftigten
+            vorherzusagen und zu bestimmen, welche Faktoren im Berufsalltag den stärksten Einfluss haben?
+            
+            **Wichtig:** Es handelt sich hierbei um einen synthetischen Datensatz, 
+            ob die Daten auf realen Erhebungen basieren, ist nicht bekannt.
+        """)
+st.markdown("""---""")
+st.subheader("📋 Projektübersicht")
+st.markdown("""
+            1. 📊 **Analyse** - Qualität der vorliegenden Daten
+            2. 📈 **Visualisierung** - Explorative Datenanalyse
+            3. 🤖 **Maschine Learning** - Maschine Learning Modellierung und Vorhersage
+            """)
 
 st.markdown("""---""")
-st.markdown("""### Hilfsangebote""")
+st.subheader("ℹ️ Informationen zum Datensatz")
 
+col1, col2, col3, col4 = st.columns(4)
 
-encoded_116117 = get_base64_encoded_image("assets/logo_116117.svg")
+with col1:
+    st.metric(
+        label="Personenanzahl",
+        value=len(df)
+    )
 
-col1, col2 = st.columns([1, 8])
+with col2:
+    st.metric(
+        label="Features",
+        value=len(df.columns)
+    )
+
+with col3:
+    st.metric(
+        label="Anzahl verschiedener Berufe",
+        value= len(df["job_role"].value_counts().values)    
+    )
+
+with col4:
+    st.metric(
+        label="Anzahl verschiedener Branchen",
+        value= len(df["industry"].value_counts().values)
+    )
+
+st.markdown("""\n""")
+
+left, right = st.columns([1, 1])
+with left:
+    st.markdown(
+        "<h4 style='text-align: center;'>Verteilung der Geschlechter</h4>",
+        unsafe_allow_html=True
+    )
+    if "gender" in df.columns:
+       
+
+        gender_counts = (
+            df["gender"]
+            .value_counts()
+            .reset_index()
+        )
+
+        gender_counts.columns = ["gender", "count"]
+
+        fig_gender = px.pie(
+            gender_counts,
+            values="count",
+            names="gender",
+            hole=0.5
+        )
+
+        fig_gender.update_layout(
+            margin=dict(l=0, r=0, t=0, b=0),
+            height=200
+        )
+
+        st.plotly_chart(
+            fig_gender,
+            use_container_width=True
+        )
+
+    else:
+        st.info("Keine Gender-Spalte gefunden.")
+with right:
+    if "seniority_level" in df.columns:
+        st.markdown(
+            "<h4 style='text-align: center;'>Verteilung hierarchischer Positionen</h4>",
+            unsafe_allow_html=True
+        )
+
+        seniority_level_counts = (
+            df["seniority_level"]
+            .value_counts()
+            .reset_index()
+        )
+
+        seniority_level_counts.columns = ["seniority_level", "count"]
+
+        fig_seniority_level = px.pie(
+            seniority_level_counts,
+            values="count",
+            names="seniority_level",
+            hole=0.5
+        )
+
+        fig_seniority_level.update_layout(
+            margin=dict(l=0, r=0, t=0, b=0),
+            height=200
+        )
+
+        st.plotly_chart(
+            fig_seniority_level,
+            use_container_width=True
+        )
+
+    else:
+        st.info("Keine Seniority-Level-Spalte gefunden.")
+
+st.markdown("""\n""")
+
+st.expander("💾 Datensatz anzeigen").dataframe(df.head())
+
+st.markdown("""---""")
+st.subheader("📚 Hilfsangebote")
+
+col1, col2 = st.columns([1, 2])
 
 with col1:
     st.markdown(f"""
         <a href="https://www.116117.de/de/psychotherapie.php" target="_blank">
-            <img src="data:image/svg+xml;base64,{encoded_116117}" width="200"/>
+            <img src="data:image/svg+xml;base64,{get_base64_encoded_image("assets/logo_116117.svg")}" width="200"/>
+        </a>
+    """, unsafe_allow_html=True)
+    st.markdown(""" \n""")
+    st.markdown(f"""
+        <a href="https://www.deutsche-depressionshilfe.de/" target="_blank">
+            <img src="data:image/svg+xml;base64,{get_base64_encoded_image("assets/stiftung-deutsche-depressionshilfe-und-suizidpraevention-logo.svg")}" width="200"/>
         </a>
     """, unsafe_allow_html=True)
 with col2:
@@ -27,9 +160,15 @@ with col2:
         **116117**: Unter der Nummer 116117 können Sie rund um die Uhr einen Termin bei einem Psychotherapeuten vereinbaren. 
         Es handelt sich um eine kostenlose Service-Hotline, die von der Kassenärztlichen Bundesvereinigung betrieben wird.
     """)
+    st.markdown(""" \n""")
+    st.markdown("""
+        **Deutsche Depressionshilfe**: Die Stiftung Deutsche Depressionshilfe bietet umfangreiche Informationen, Beratung und Unterstützung für Menschen, die von Depressionen betroffen sind, sowie für deren Angehörige.
+    """)
+
+
 
 st.markdown("""---""")
-st.markdown("""### Ressourcen & Downloads""")
+st.subheader("""⬇️ Ressourcen & Downloads""")
 
 encoded_github = get_base64_encoded_image("assets/github.png")
 encoded_kaggle = get_base64_encoded_image("assets/kaggle.png")
@@ -108,3 +247,7 @@ with col3:
             "</p>",
             unsafe_allow_html=True
         )
+
+
+st.markdown("""---""")
+st.markdown("""<span style="font-size: 0.85rem; color: #555a;">DataPy SoSe26 - Mental Health in Tech Project</span>""", unsafe_allow_html=True)
