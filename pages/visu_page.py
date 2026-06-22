1ab464632829cc6c83a59bd5b35d826bfe5764da
hpbexxter <mail@hannes.pralle.de>
2026-06-22 15:52:53 +0200

    feat(charts): added missing charts

--- pages/visu_page.py
+++ pages/visu_page.py
@@ -19,7 +19,8 @@ if df is None:
             """)
 else:
     # Tab Section
-    tab1, tab2, tab3 = st.tabs(["Verteilungen", "Korrelationen", "Vergleiche"])
+    tab1, tab2, tab3, tab4 = st.tabs(["Verteilungen", "Korrelationen", "Kerndichte", "Vergleiche"])
+    feature_cols = [col for col in df.columns if "score" in col and col != "phq9_score"]
 
     with tab1:
         st.subheader("Verteilungen der Features")
@@ -38,7 +39,7 @@ else:
         st.plotly_chart(fig)
 
     with tab2:
-        st.subheader("Korrelationsmatrix")
+        st.subheader("Korrelationsmatrix aller numerischen Features")
         corr = df.select_dtypes(include='number').corr()
 
         fig = ff.create_annotated_heatmap(
@@ -64,8 +65,10 @@ else:
 
         st.markdown("""---""")
 
-        st.subheader("Heatmap der Korrelationen")
-        phq_corr = df.select_dtypes(include='number').corr()[['phq9_score']].drop('phq9_score').sort_values('phq9_score')
+        st.subheader("Heatmap der Feature-Korrelationen mit PHQ-9")
+        
+        # Korrelationen mit PHQ-9 Score berechnen und sortieren
+        phq_corr = df.select_dtypes(include='number').corr()[['phq9_score']].drop('phq9_score').sort_values('phq9_score', ascending=False)
 
         fig, ax = plt.subplots(figsize=(3, 5))
         sns.heatmap(
@@ -77,10 +80,64 @@ else:
             vmax=1,
             ax=ax
         )
-        ax.set_title("Korrelationen mit PHQ-9")
+        st.pyplot(fig)
+
+        st.markdown("""---""")
+        st.subheader("Durchschnittlicher PHQ-9 Score nach Jobrolle und Senioritätslevel")
+        pivot = df.pivot_table(
+        values="phq9_score",
+        index="job_role",
+        columns="seniority_level",
+        aggfunc="mean"
+        )
+
+        fig, ax = plt.subplots(figsize=(10,6))
+        sns.heatmap(
+            pivot,
+            annot=True,
+            fmt=".1f",          
+            cmap="Reds",
+            vmin=0,
+            vmax=27,
+            ax=ax
+        )
         st.pyplot(fig)
         
     with tab3:
-        st.subheader("Vergleiche zwischen Gruppen")
-        st.write("Hier können Sie Vergleiche zwischen verschiedenen Gruppen innerhalb Ihrer Daten visualisieren. Wählen Sie ein Feature aus, um die entsprechenden Gruppenvergleiche anzuzeigen.")
-    
+        st.subheader("Kerndichte der Scores")
+        
+        selected_kde_first = st.selectbox("Erstes Feature", feature_cols, key="kde_first", index=0)
+        selected_kde_second = st.selectbox("Zweites Feature", feature_cols, key="kde_second", index=1)
+
+        samples = st.slider("Samples im Diagramm", min_value=100, max_value=len(df), value=10000, step=100)
+
+        fig, ax = plt.subplots()
+        sns.kdeplot(
+            data=df.sample(samples),
+            x=selected_kde_first,
+            y=selected_kde_second,
+            fill=True,
+            cmap="Reds"
+        )
+        st.pyplot(fig)
+
+    with tab4:
+
+        st.subheader("Vergleich von Features")
+
+        selected_hex_first = st.selectbox("Erstes Feature", feature_cols, key="hex_first", index=0)
+        selected_hex_second = st.selectbox("Zweites Feature", feature_cols, key="hex_second", index=1)
+
+        fig, ax = plt.subplots()
+        hb = ax.hexbin(
+            df[selected_hex_first],
+            df[selected_hex_second],
+            C=df["phq9_score"],
+            reduce_C_function=np.mean,
+            gridsize=30,
+            cmap="RdYlGn_r"
+        )
+        plt.colorbar(hb, label="Ø PHQ-9")
+        ax.set_xlabel(selected_hex_first)
+        ax.set_ylabel(selected_hex_second)
+        st.pyplot(fig)
